a8ffe341dda996c6482894115e7bd30c42ae0e2f
mspil88 <matthewspillane88@gmail.com>
2022-05-30 17:52:15 +0100

    proportion in missing

--- src/eda/missing.py
+++ src/eda/missing.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 def calculate_missing_values(df, shape):
     missing = {}
+    length = shape[1]
 
     for column in df:
         missing_count = df[column].isna().sum()
-        missing[column] = {"count": missing_count, "proportion": missing_count/shape[1]}
+        missing[column] = {"count": missing_count, "proportion": 0 if length == 0 else missing_count/length}
 
     return missing
