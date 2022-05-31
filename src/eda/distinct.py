a6d2f6b8d5333bff89181efca5098a07829fdc54
mspil88 <matthewspillane88@gmail.com>
2022-05-31 15:38:13 +0100

    return dict fix and type hints

--- src/eda/distinct.py
+++ src/eda/distinct.py
@@ -1,16 +1,18 @@
-from typing import Dict
 import pandas as pd
 import numpy as np
 
 
-def count_distinct(column: pd.Series) -> Dict:
+def count_distinct(column: pd.Series, column_name: str) -> dict:
     distinct_values = column.nunique()
 
-    return {"distinct": {"count": distinct_values,
-                         "proportion": distinct_values/len(column)}}
+    return {column_name:
+                {"distinct": {"count": distinct_values,
+                              "proportion": distinct_values / len(column)}}}
 
-def count_zero_values(column: pd.Series) -> Dict:
+
+def count_zero_values(column: pd.Series, column_name: str) -> dict:
     num_zero = np.array(column == 0)
 
-    return {"zeros": {"count": np.sum(num_zero),
-                      "proportion": num_zero/len(num_zero)}}
+    return {column_name:
+                {"zeros": {"count": np.sum(num_zero),
+                           "proportion": num_zero / len(num_zero)}}}
