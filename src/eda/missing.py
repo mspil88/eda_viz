7dbd631eb519fef0bff7cbdc8e0992e197423d8e
mspil88 <matthewspillane88@gmail.com>
2022-05-31 09:48:01 +0100

    table level missing info

--- src/eda/missing.py
+++ src/eda/missing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def calculate_missing_values(df, shape):
@@ -10,6 +11,15 @@ def calculate_missing_values(df, shape):
         missing[column] = {"count": missing_count, "proportion": 0 if length == 0 else missing_count/length}
 
     return missing
+
+
+def df_level_missing_values(missing_dict, shape):
+    counts = np.array([v['count'] for k, v in missing_dict.items()])
+    missing_cells = np.sum(counts)
+    missing_cells_prop = missing_cells/(shape[0]*shape[1])
+    vars_with_missing = np.sum(counts > 0.0)
+
+    return missing_cells, missing_cells_prop, vars_with_missing
 
 
 def missing_plot(missing):
