3c808b7f5f29f7b5ef740b0038a51502df9d5ac5
mspil88 <matthewspillane88@gmail.com>
2022-05-31 15:37:30 +0100

    return dict and type hints added

--- src/eda/missing.py
+++ src/eda/missing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def calculate_missing_values(df, shape):
+def calculate_missing_values(df: pd.DataFrame, shape: tuple) -> dict:
     missing = {}
     length = shape[1]
 
@@ -13,13 +13,18 @@ def calculate_missing_values(df, shape):
     return missing
 
 
-def df_level_missing_values(missing_dict, shape):
+def df_level_missing_values(missing_dict: dict, shape: tuple) -> dict:
     counts = np.array([v['count'] for k, v in missing_dict.items()])
     missing_cells = np.sum(counts)
     missing_cells_prop = missing_cells/(shape[0]*shape[1])
     vars_with_missing = np.sum(counts > 0.0)
 
-    return missing_cells, missing_cells_prop, vars_with_missing
+    return {"overview": {"missing_cells": missing_cells, "missing_proportion": missing_cells_prop,
+                         "variables_with_missing": vars_with_missing}}
+
+
+def reformat_missing(missing_dict: dict) -> list:
+    return [{k: {"missing": v}} for k, v in missing_dict.items()]
 
 
 def missing_plot(missing):
