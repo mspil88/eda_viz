bdc795db4526114a99aedf325c2054ad9f020708
mspil88 <matthewspillane88@gmail.com>
2022-05-31 15:40:37 +0100

    return dict fixed and type hints added

--- src/eda/duplicates.py
+++ src/eda/duplicates.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 
-def get_duplicates(df, shape):
+def get_duplicates(df: pd.DataFrame, shape: tuple) -> dict:
     num_duplicates = df.duplicated().sum()
 
-    return {"duplicates": {"num_duplicates": num_duplicates,
-                           "proportion duplicated": num_duplicates / shape[0]}}
+    return {"overview": {"duplicates": {"num_duplicates": num_duplicates,
+                                                      "proportion duplicated": num_duplicates / shape[0]}}}
