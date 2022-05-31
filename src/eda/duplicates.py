bf95c07634c7c8e506d250486746c298af6be55a
mspil88 <matthewspillane88@gmail.com>
2022-05-31 15:23:32 +0100

    nested return value dict

--- src/eda/duplicates.py
+++ src/eda/duplicates.py
@@ -4,5 +4,5 @@ import pandas as pd
 def get_duplicates(df, shape):
     num_duplicates = df.duplicated().sum()
 
-    return {"num_duplicates": num_duplicates,
-            "proportion duplicated": num_duplicates/shape[0]}
+    return {"duplicates": {"num_duplicates": num_duplicates,
+                           "proportion duplicated": num_duplicates / shape[0]}}
