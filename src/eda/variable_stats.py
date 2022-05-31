a1aef54d791a0941d1dbe42f3a0196b758f23da8
mspil88 <matthewspillane88@gmail.com>
2022-05-31 15:09:43 +0100

    numeric and categorical variable stats

---
+++ src/eda/variable_stats.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+
+def numeric_statistics(column: pd.Series):
+    _min = column.min()
+    _max = column.max()
+    q25 = np.percentile(column, 25)
+    q75 = np.percentile(column, 75)
+    return {"stats":
+            {"mean": column.mean(),
+             "variance": column.var(),
+             "std": column.std(),
+             "min": _min,
+             "Q25": q25,
+             "median": column.median(),
+             "Q75": q75,
+             "max": _max,
+             "range": _max - _min,
+             "IQR": q75-q25,
+             "skew": column.skew(),
+             "kurtosis": column.kurt(),
+             "sum": np.sum(column)
+    }}
+
+
+def categorical_statistics(column: pd.Series):
+    return {"stats":
+                {"mode": column.mode[0]}}
