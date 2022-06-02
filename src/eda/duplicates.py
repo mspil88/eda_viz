39a1382898c24ed6d8eb142c345f94b27b5de413
mspil88 <matthewspillane88@gmail.com>
2022-06-02 18:27:43 +0100

    reverted back to old object structure

--- src/eda/duplicates.py
+++ src/eda/duplicates.py
@@ -4,5 +4,5 @@ import pandas as pd
 def get_duplicates(df: pd.DataFrame, shape: tuple) -> dict:
     num_duplicates = df.duplicated().sum()
 
-    return {"overview": {"duplicates": {"num_duplicates": num_duplicates,
-                                        "proportion_duplicated": num_duplicates / shape[0]}}}
+    return {"duplicates": {"num_duplicates": num_duplicates,
+                                        "proportion_duplicated": num_duplicates / shape[0]}}
