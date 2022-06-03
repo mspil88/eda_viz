262a2ae48046ca66cfec2ae8ee93372516f9d062
mspil88 <matthewspillane88@gmail.com>
2022-06-03 10:07:04 +0100

    changed shape argument to num rows

--- src/eda/duplicates.py
+++ src/eda/duplicates.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 
-def get_duplicates(df: pd.DataFrame, shape: tuple) -> dict:
+def get_duplicates(df: pd.DataFrame, n_rows: int) -> dict:
     num_duplicates = df.duplicated().sum()
 
     return {"duplicates": {"num_duplicates": num_duplicates,
-                                        "proportion_duplicated": num_duplicates / shape[0]}}
+                                        "proportion_duplicated": num_duplicates / n_rows}}
