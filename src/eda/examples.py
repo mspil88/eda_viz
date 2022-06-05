b98e691aacbee5e41e579156db535c8bb6671fe9
mspil88 <matthewspillane88@gmail.com>
2022-06-05 11:39:58 +0100

    stringified nans

--- src/eda/examples.py
+++ src/eda/examples.py
@@ -8,12 +8,15 @@ class Example:
 
 
 def get_example(df, example_type, **kwargs):
-    if example_type == "head":
-        return Example("head", df.head(**kwargs))
-    elif example_type == "tail":
-        return Example("tail", df.tail(**kwargs))
-    elif example_type == "sample":
-        return Example("sample", df[~df.isna()].sample(**kwargs))
+    if isinstance(df, pd.DataFrame):
+        if example_type == "head":
+            return Example("head", df.head(**kwargs).fillna("nan").to_dict())
+        elif example_type == "tail":
+            return Example("tail", df.tail(**kwargs).fillna("nan").to_dict())
+        elif example_type == "sample":
+            return Example("sample", df[~df.isna()].sample(**kwargs).to_dict())
+    elif isinstance(df, pd.Series):
+        return Example("sample", list(df[~df.isna()].sample(**kwargs)))
 
 
 
