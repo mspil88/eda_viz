3f45870299f5093077b39ec3e9018815f4ee8a40
mspil88 <matthewspillane88@gmail.com>
2022-06-07 14:24:37 +0100

    changed object key names to count and proportion

--- src/eda/duplicates.py
+++ src/eda/duplicates.py
@@ -4,5 +4,5 @@ import pandas as pd
 def get_duplicates(df: pd.DataFrame, n_rows: int) -> dict:
     num_duplicates = df.duplicated().sum()
 
-    return {"duplicates": {"num_duplicates": num_duplicates,
-                                        "proportion_duplicated": num_duplicates / n_rows}}
+    return {"duplicates": {"count": num_duplicates,
+                                        "proportion": num_duplicates / n_rows}}
