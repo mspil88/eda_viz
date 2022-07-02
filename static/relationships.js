890efca2ca8113aca0bebf8d244661fcaf99aea3
mspil88 <matthewspillane88@gmail.com>
2022-07-02 10:09:50 +0100

    implemented scatter plot btn select

--- static/relationships.js
+++ static/relationships.js
@@ -13,7 +13,7 @@ const toggleOff = (elem) => {
     elem.style.color = "#808080";
 };
 
-const toggleOffRemainder = (elem) => {
+const toggleOffRemainder = (elem, btns) => {
     for(let i of corrBtns) {
         if(i.className.split(" ")[1] !== elem) {
             toggleOff(i);
@@ -21,20 +21,25 @@ const toggleOffRemainder = (elem) => {
     }
 }
 
+//too repetitive
 function controlCorrelationView() {
     const btnElem = this.className.split(" ")[1];
     if(btnElem === "pearson") {
         toggleOn(this);
         toggleOffRemainder(btnElem);
+        renderCorrelationPlot(obj, btnElem)
     } else if(btnElem === "spearman") {
         toggleOn(this);
         toggleOffRemainder(btnElem);
+        renderCorrelationPlot(obj, btnElem)
     } else if(btnElem === "kendall") {
         toggleOn(this);
         toggleOffRemainder(btnElem);
+        renderCorrelationPlot(obj, btnElem)
     } else if(btnElem === "cramer") {
         toggleOn(this);
         toggleOffRemainder(btnElem);
+        renderCorrelationPlot(obj, btnElem)
     }
 
 }
@@ -42,6 +47,8 @@ function controlCorrelationView() {
 corrBtns.forEach((btn) => {
     btn.addEventListener("click", controlCorrelationView);
 })
+
+
 
 const createCorrelationData = (obj) => {
     const x = Object.keys(obj);
@@ -54,18 +61,64 @@ const createCorrelationData = (obj) => {
     return [x, y, z]
 }
 
-const [x_ax, y_ax, z_ax] = createCorrelationData(obj);
-
-const correlationTrace =
-    [{z: z_ax,
-    x: x_ax,
-    y: y_ax,
-    colorscale: 'Greys',
-    type: 'heatmap',
-    hoverongaps: false}]
 
 
-const plotConfig = {responsive: true, displayModeBar: false}
 
 
-Plotly.newPlot(correlationPlot, correlationTrace, config=plotConfig);
+
+
+
+
+const renderCorrelationPlot = (obj, corrType) => {
+
+    const [x_ax, y_ax, z_ax] = createCorrelationData(obj[corrType]);
+
+    const correlationTrace =
+        [{z: z_ax,
+        x: x_ax,
+        y: y_ax,
+        colorscale: 'Greys',
+        type: 'heatmap',
+        hoverongaps: false}]
+
+    const plotConfig = {responsive: true, displayModeBar: false}
+    Plotly.newPlot(correlationPlot, correlationTrace, config=plotConfig);
+
+}
+
+const scatterRow1 = Array.from(document.querySelector(".scatter-row1").children);
+const scatterRow2 = Array.from(document.querySelector(".scatter-row2").children);
+
+
+function controlScatterView1() {
+    const btnElem = this.className.split(" ")[1];
+    console.log(`clicked ${btnElem}`)
+    toggleOn(this);
+    toggleOffScatter(btnElem, scatterRow1);
+
+}
+
+function controlScatterView2() {
+    const btnElem = this.className.split(" ")[1];
+    console.log(`clicked ${btnElem}`)
+    toggleOn(this);
+    toggleOffScatter(btnElem, scatterRow2);
+
+}
+
+const toggleOffScatter = (elem, row) => {
+    for(let i of row) {
+        if(i.className.split(" ")[1] !== elem) {
+            toggleOff(i);
+        }
+    }
+}
+scatterRow1.forEach((btn) => {
+    btn.addEventListener("click", controlScatterView1);
+
+});
+
+scatterRow2.forEach((btn) => {
+    btn.addEventListener("click", controlScatterView2);
+});
+
